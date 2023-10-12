2e6ef830f941d9117b1da450202848d13bda3a17
PYTHONLL-kang <dlrkd0987@gmail.com>
2023-10-12 21:09:43 +0900

    edit: add chat validator, fix status code

--- backend/controllers/chat/send.go
+++ backend/controllers/chat/send.go
@@ -5,30 +5,37 @@ import (
 	"github.com/google/uuid"
 	"implude.kr/VOAH-Official-Message/database"
 	"implude.kr/VOAH-Official-Message/models"
+	"implude.kr/VOAH-Official-Message/utils/validator"
 )
 
 type Message struct {
-	content   string
-	ChannelId uuid.UUID
+	Content   string    `gorm:"type:text;not null;"`
+	ChannelId uuid.UUID `gorm:"type:uuid;not null;"`
 }
 
-func sendJson(c *fiber.Ctx) {
+func sendJson(c *fiber.Ctx) error {
 	body := new(Message)
-
 	if err := c.BodyParser(body); err != nil {
 		return c.Status(400).JSON(fiber.Map{
 			"message": "Invalid request",
 		})
 	}
 
+	if errArr := validator.VOAHValidator.Validate(body); len(errArr) != 0 {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Invalid request",
+			"error":   errArr,
+		})
+	}
+
 	db := database.DB
 	chat := models.Chat{
-		Content:   body.content,
+		Content:   body.Content,
 		ChannelID: body.ChannelId,
 	}
 
 	if err := db.Create(&chat).Error; err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not create user", "data": err})
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not create chat", "data": err})
 	}
 
 	return c.Status(201).JSON(fiber.Map{"status": "success"})
