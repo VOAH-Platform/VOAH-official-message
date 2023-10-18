437d866c1dd48213c69ff5ef928ba17085f95105
samuel5721 <samuel20070731@gmail.com>
2023-10-18 22:45:02 +0900

    fix: markdown detail

--- frontend/src/components/GhostInput/index.tsx
+++ frontend/src/components/GhostInput/index.tsx
@@ -28,21 +28,19 @@ export function GhostInput({
     tag2: string,
   ): string {
     const escapedSubject = subject.replace(/([.*+?^${}()|[\]\\])/g, '\\$1');
-    let replacedStr = input;
-    let count = 0;
 
-    replacedStr = replacedStr.replace(RegExp(escapedSubject, 'g'), (match) => {
-      count++;
-      return count % 2 === 1 ? match + tag1 : tag2 + match;
-    });
-
-    // 만약 ~~ 가 홀수번 등장하면, 마지막 <s>를 ~~ 로 다시 바꿔줌
-    if (count % 2 !== 0) {
-      replacedStr = replacedStr.replace(
-        RegExp(tag2 + escapedSubject + '(?!.*' + tag2 + escapedSubject + ')'),
-        subject,
+    if (tag2 === '') {
+      const pattern = new RegExp(
+        escapedSubject + '([^' + escapedSubject + ']+)',
+        'g',
       );
+      return input.replace(pattern, '<b>' + escapedSubject + '$1</b>');
     }
+
+    const pattern = new RegExp(escapedSubject + '(.*?)' + escapedSubject, 'g');
+    let replacedStr = input.replace(pattern, (match, innerContent) => {
+      return subject + tag1 + innerContent + tag2 + subject;
+    });
 
     // <s></s> 사이에 아무것도 없으면 다시 ~~로 바꿔줌
     replacedStr = replacedStr.replace(
@@ -62,6 +60,9 @@ export function GhostInput({
       italic3: ['_', '<i>', '</i>'],
       swung: ['~~', '<s>', '</s>'],
       underLine: ['__', '<u>', '</u>'],
+      h3: ['### ', '###', ''],
+      h2: ['## ', '##', ''],
+      h1: ['# ', '#', ''],
     };
     for (const i of Object.keys(replacingMap)) {
       str = replaceStr(
