55a1a378489461f35d9b7ab5e908400c173a5c8f
samuel5721 <samuel20070731@gmail.com>
2023-10-18 22:22:48 +0900

    feat: ghost input markdown frame adding

--- frontend/src/components/GhostInput/index.tsx
+++ frontend/src/components/GhostInput/index.tsx
@@ -21,8 +21,62 @@ export function GhostInput({
       return;
     };
 
+  function replaceStr(
+    input: string,
+    subject: string,
+    tag1: string,
+    tag2: string,
+  ): string {
+    const escapedSubject = subject.replace(/([.*+?^${}()|[\]\\])/g, '\\$1');
+    let replacedStr = input;
+    let count = 0;
+
+    replacedStr = replacedStr.replace(RegExp(escapedSubject, 'g'), (match) => {
+      count++;
+      return count % 2 === 1 ? match + tag1 : tag2 + match;
+    });
+
+    // 만약 ~~ 가 홀수번 등장하면, 마지막 <s>를 ~~ 로 다시 바꿔줌
+    if (count % 2 !== 0) {
+      replacedStr = replacedStr.replace(
+        RegExp(tag2 + escapedSubject + '(?!.*' + tag2 + escapedSubject + ')'),
+        subject,
+      );
+    }
+
+    // <s></s> 사이에 아무것도 없으면 다시 ~~로 바꿔줌
+    replacedStr = replacedStr.replace(
+      RegExp(escapedSubject + tag1 + tag2 + escapedSubject, 'g'),
+      subject + subject,
+    );
+
+    return replacedStr;
+  }
+
+  function processing(str: string): string {
+    const replacingMap: {
+      [key: string]: string[];
+    } = {
+      bold: ['**', '<b>', '</b>'],
+      italic: ['*', '<i>', '</i>'],
+      italic3: ['_', '<i>', '</i>'],
+      swung: ['~~', '<s>', '</s>'],
+      underLine: ['__', '<u>', '</u>'],
+    };
+    for (const i of Object.keys(replacingMap)) {
+      str = replaceStr(
+        str,
+        replacingMap[i][0],
+        replacingMap[i][1],
+        replacingMap[i][2],
+      );
+    }
+    return str;
+  }
+
   useEffect(() => {
     console.log(input.split('\n'));
+    console.log(processing(input.split('\n')[0]));
     setInputHeight(divRef.current?.offsetHeight!);
     setInputWidth(divRef.current?.offsetWidth!);
   }, [divRef, divRef.current, input]);
