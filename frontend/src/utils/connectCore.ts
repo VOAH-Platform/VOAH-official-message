64be4cf2f5189523f1328d264a88cc9e9e5ab9b8
Yim Hoseon <justin.yim14@gmail.com>
2023-10-23 08:15:48 +0900

    feat: Connect to core

---
+++ frontend/src/utils/connectCore.ts
@@ -0,0 +1,29 @@
+// const { port1, port2 } = new MessageChannel();
+
+// const voahMessages = user;
+
+// window.addEventListener('message');
+
+// window.parent.postMessage
+
+// export const connectCore = (e: MessageEvent) => {
+//   if (e.origin != 'localhost:8080') {
+//   }
+//   port1.onmessage = (
+//     e: MessageEvent<{
+//       type: string;
+//       data: unknown;
+//     }>,
+//   ) => {
+//     console.log(e.data);
+//     switch (e.data.type) {
+//       case 'VOAH__FRAME_INIT':
+//         voah;
+//         voahMessages.frame.initDone(url);
+//         break;
+//       case 'VOAH__USER_GET_TOKEN':
+//         voahMessages.user.getToken(user.accessToken);
+//         break;
+//     }
+//   };
+// };
